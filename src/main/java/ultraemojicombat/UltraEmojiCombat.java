240e95ad4cf071ce428d7c87e5291f2061c3c6a3
Nathália Camargo <nathalia77.eu@hotmail.com>
2022-05-10 13:53:17 -0300

    Agregation

--- src/main/java/ultraemojicombat/UltraEmojiCombat.java
+++ src/main/java/ultraemojicombat/UltraEmojiCombat.java
@@ -15,6 +15,11 @@ public class UltraEmojiCombat {
                                 119.3f, 5, 4, 3);
         l[5] = new Lutador ("Nerdart", "EUA", 30, 1.81f, 
                                 105.7f, 12, 2, 4);
-        l[5].status();
+        
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar();
+        l[0].status();
+        l[1].status();
     }
 }
